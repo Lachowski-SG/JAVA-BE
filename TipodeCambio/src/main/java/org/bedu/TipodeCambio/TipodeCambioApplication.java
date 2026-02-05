12f782bb012b8a8b8297c186e4a0bc4bd442ca40
Lachowski-SG <sagahon.85@hotmail.com>
2026-02-05 16:46:44 -0600

    TipoDeCambio

---
+++ TipodeCambio/src/main/java/org/bedu/TipodeCambio/TipodeCambioApplication.java
@@ -0,0 +1,13 @@
+package org.bedu.TipodeCambio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipodeCambioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipodeCambioApplication.class, args);
+	}
+
+}
