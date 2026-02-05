d6a0912aee42f9f360c3fcaa8148d36a979ec79a
Lachowski-SG <sagahon.85@hotmail.com>
2026-02-05 16:11:12 -0600

    teste

---
+++ TipodeCambio/src/main/java/org/bedu/TipodeCambio/controller/TipoCambioController.java
@@ -0,0 +1,9 @@
+package org.bedu.TipodeCambio.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class TipoCambioController {
+
+}
---
+++ gradleDemo/app/src/main/java/bedu/Cajero.java
@@ -0,0 +1,24 @@
+package bedu;
+
+public class Cajero {
+//Cliente de la base de datos
+    private Cliente cliente;
+
+    public Cajero(Cliente cliente){
+        this.cliente=cliente;
+    }
+//Preguntar NIP
+public boolean validarNip(String nip){
+    if (nip.equals(cliente.getNip())){
+        return true;        
+    } else{
+        return false;
+    }
+// return nip.equals(cliente.getNip());
+}
+//Abonar
+public void abonar(int  monto){
+    cliente.setSaldo(cliente.getSaldo()+ monto);
+}
+//Retirar
+}
